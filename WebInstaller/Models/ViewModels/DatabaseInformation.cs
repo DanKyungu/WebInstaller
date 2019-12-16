de75a6f4ee0788c38a3536866f03329dfaf6acc2
Dan Kyungu <kyungu.dan@hotmail.com>
2019-12-16 15:54:46 +0200

    Ajout du modele DatabaseInformation

---
+++ WebInstaller/Models/ViewModels/DatabaseInformation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebInstaller.Models.ViewModels
+{
+    public class DatabaseInformation
+    {
+        public string ServerName { get; set; }
+
+        public string DatabaseName { get; set; }
+
+        public string  ServerUserName { get; set; }
+
+        public string ServerPassword { get; set; }
+
+        public string ServerPort { get; set; }
+    }
+}
