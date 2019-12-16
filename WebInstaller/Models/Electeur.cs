32ce2b32624008ad44b88036ba4e21e2d06022ec
Dan Kyungu <kyungu.dan@hotmail.com>
2019-12-16 15:42:00 +0200

    Ajout du model Electeur

---
+++ WebInstaller/Models/Electeur.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebInstaller.Models
+{
+    public class Electeur
+    {
+        public string Nom { get; set; }
+
+        public string Postnom { get; set; }
+
+        public string  Prenom { get; set; }
+
+    }
+}
