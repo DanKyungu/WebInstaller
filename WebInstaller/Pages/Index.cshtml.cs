83197c08c163c0b17d82378028e4856642bdc725
Dan Kyungu <kyungu.dan@hotmail.com>
2019-12-16 16:10:10 +0200

    Correction envoie des données

--- WebInstaller/Pages/Index.cshtml.cs
+++ WebInstaller/Pages/Index.cshtml.cs
@@ -5,11 +5,16 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using WebInstaller.Models.ViewModels;
 
 namespace WebInstaller.Pages
 {
     public class IndexModel : PageModel
     {
+        [BindProperty]
+        public DatabaseInformation DatabaseInformation { get; set; } = new DatabaseInformation();
+
+
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
@@ -24,6 +29,7 @@ namespace WebInstaller.Pages
 
         public IActionResult OnPostAsync()
         {
+            Console.WriteLine(DatabaseInformation.ServerName);
             return RedirectToPage("Index");
         }
     }
