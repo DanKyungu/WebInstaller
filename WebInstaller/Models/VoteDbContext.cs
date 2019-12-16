d018fb07eb572cbd12640efbd8aa937fadd170a8
Dan Kyungu <kyungu.dan@hotmail.com>
2019-12-16 15:44:56 +0200

    Installation de entity framework core

--- WebInstaller/Models/VoteDbContext.cs
+++ WebInstaller/Models/VoteDbContext.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace WebInstaller.Models
 {
-    public class VoteDbContext
+    public class VoteDbContext : DbContext
     {
     }
 }
